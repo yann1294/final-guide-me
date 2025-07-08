ae48c1bfcaf6d621549ce2f143b00f59111eb487
Choco1204 <unlimitedwebworks7@icloud.com>
2025-07-08 15:24:26 +0530

    fix:src/app/api/packages/[id]/route.ts:12:104 Type error: Expected 2 arguments, but got 3

--- src/app/api/packages/[id]/route.ts
+++ src/app/api/packages/[id]/route.ts
@@ -3,15 +3,25 @@ import { handleDELETE, handleGET, handlePATCH } from "@/lib/utils/apiHandler";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, { params }: any) {
-  return await handleGET(`${process.env.NEXT_PUBLIC_BACKEND_URL}/packages/${params.id}`);
+  return await handleGET(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/packages/${params.id}`,
+  );
 }
 
-export async function PATCH(req: Request) {
-  let body: any = await req.json();
-  console.log(body);
-  return await handlePATCH(`${process.env.NEXT_PUBLIC_BACKEND_URL}/packages/${body['id']}/tours`, req, JSON.stringify(body));
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  // let body: any = await req.json();
+  // console.log(body);
+  return await handlePATCH(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/packages/${params.id}/tours`,
+    req,
+  );
 }
 
 export async function DELETE(req: Request, { params }: any) {
-  return await handleDELETE(`${process.env.NEXT_PUBLIC_BACKEND_URL}/packages/${params.id}`)
+  return await handleDELETE(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/packages/${params.id}`,
+  );
 }
